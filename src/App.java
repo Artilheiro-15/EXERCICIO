ed8c61f26331719a590366813820f329d32e1604
Atila jr <atilla.junior33@gmail.com>
2022-12-08 14:15:08 -0300

    Cod Finalizado

--- src/App.java
+++ src/App.java
@@ -11,7 +11,7 @@ import entities.Worker;
 public class App {
     public static void main(String[] args) throws Exception {
 
-    
+
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
